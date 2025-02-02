bb00c28bd1acb98355dba0def4961547327c9e57
Karim <konanfoi@gmail.com>
2025-02-01 23:19:51 -0500

    Task 2 - Modifying Customer Orders

---
+++ cc_3.js
@@ -0,0 +1,12 @@
+//Task 1: Product Price Management
+
+let prices = [70, 80, 90, 100, 110];
+prices.push(120); //Add a new price to array
+prices.shift(); // Remove first price
+console.log("Updated Prices:", prices); //Log updated price list
+
+
+//Task 2: Modifying Customer Orders
+
+let orders = [50, 60, 70, 80, 90]; //Declare an array
+
